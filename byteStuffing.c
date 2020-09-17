1be3bbf615bfb67a59589a52882f5d5a3d932508
mahimareddem10 <53556607+mahima-reddem@users.noreply.github.com>
2020-09-17 12:39:54 +0530

    Update byteStuffing.c

--- byteStuffing.c
+++ byteStuffing.c
@@ -12,14 +12,26 @@ int main(){
     printf("Enter the string to be inserted:");
     scanf("%s",temp);
     printf("%s ",start);
+    //printf("%s",temp);
     
     int i=0;
     while(i<strlen(str)){
-        int j = 0, c = 0, k = i;
+        int j = 0,l =0, c = 0, k = i, c1 = 0;
         while(j<strlen(start)){
             if(str[k] == start[j] || str[k] == end[j]){
                 c++;
             }
+            int m=i,n=0;
+            while(m<strlen(temp)+i){
+                if(str[m] == temp[n]){
+                    c1++;
+                }
+                m++;
+                n++;
+            }   
+            if(c1 == strlen(temp)){
+                printf("%s",temp);
+            }     
             k++;
             j++;
         
@@ -27,7 +39,7 @@ int main(){
                printf("%s ",temp);
             } 
         }
-        
+       
         printf("%c ",str[i]);
         i++;
         
